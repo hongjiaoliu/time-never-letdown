faaf289b4bc4bb57fdbc78e457f9912c3bf85e17
liuhongjiao <liuhongjiao@koolearn.com>
2023-02-14 09:31:23 +0800

    add extend points in dubbo for build

--- docs/.vuepress/dist/assets/3-extend-point-dubbo.html-d41fd1e9.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,V as t,W as n}from"./framework-9e98e3f0.js";const o={};function _(c,r){return t(),n("div")}const a=e(o,[["render",_],["__file","3-extend-point-dubbo.html.vue"]]);export{a as default};
